a155072000b17de089606ade6e70b0a133e1152f
donchelo <mgarciap333@gmail.com>
2024-08-18 01:48:13 -0500

    [main] dark theme created

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,14 +4,22 @@ import { createTheme, ThemeOptions } from '@mui/material/styles';
 
 const themeOptions: ThemeOptions = {
   palette: {
+    mode: 'dark',  // Configura el modo oscuro
+    background: {
+      default: '#121212',  // Un tono oscuro para el fondo, como casi negro
+      paper: '#1E1E1E',  // Un color ligeramente más claro para los elementos tipo "paper" o superficies
+    },
     primary: {
-      main: '#232222',  // Pantone 2735 C
+      main: '#90CAF9',  // Un azul claro que se ve bien en un fondo oscuro
+      contrastText: '#FFFFFF',  // Texto en color blanco para contrastar
     },
     secondary: {
-      main: '#F8C2CB',  // Pantone 699 C
+      main: '#F48FB1',  // Un rosa claro para el contraste
+      contrastText: '#FFFFFF',  // Texto en color blanco para contrastar
     },
-    background: {
-      default: '#FFFFFF',  // Blanco
+    text: {
+      primary: '#FFFFFF',  // Texto principal en blanco
+      secondary: '#B0BEC5',  // Texto secundario en un gris claro
     },
   },
   typography: {
@@ -19,6 +27,7 @@ const themeOptions: ThemeOptions = {
     h1: {
       fontSize: '2rem',
       fontWeight: 700,
+      color: '#FFFFFF',  // Asegúrate de que el texto H1 sea blanco para que contraste en el fondo oscuro
     },
     // Puedes personalizar más opciones de tipografía aquí
   },
